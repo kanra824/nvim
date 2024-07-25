5ae8e71db445979da1ac0fc8e9adc4131cae5394
Yohei Maeda <yohei_maeda@r.recruit.co.jp>
2024-07-25 16:21:11 +0900

    update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -56,12 +56,4 @@ return {
         event = "InsertEnter",
         opts = {}
     },
-    {
-        'akinsho/bufferline.nvim',
-        version = "*",
-        dependencies = 'nvim-tree/nvim-web-devicons',
-        config = function()
-            require("bufferline").setup{}
-        end
-    }
 }
